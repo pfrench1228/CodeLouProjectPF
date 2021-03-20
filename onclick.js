228aae27cec83fa79c8395ed720437b2861f7e6c
Preston French <pfrench1228@gmail.com>
2021-03-20 12:40:13 -0400

    utilized classlist into onclick js

--- onclick.js
+++ onclick.js
@@ -1,33 +1,60 @@
 
+// function menuFxn() {
+//   let pxl = window.innerWidth;
+//   let a = document.getElementsByClassName("icon")[0];
+//   let b = document.getElementsByClassName("main-nav")[0];
+//   let c = document.getElementsByClassName("burg")[0];
+//   let d = document.getElementsByClassName("name")[0];
+//   let e = document.getElementsByClassName("logo")[0];
+//       if (b.style.display === "block") {
+//           a.style.width = "15%";
+//           b.style.display = "none";
+//           b.style.background = "none";
+//           c.style.maxWidth = "100%";
+//           if (pxl < 410) {
+//           d.style.width = "initial";
+//           e.style.marginLeft = "auto";
+//             }
+//         } else {
+//           a.style.width = "25%";
+//           b.style.display = "block";
+//           b.style.background = "black";
+//           b.style.borderRadius  = "15px";
+//           c.style.maxWidth = "60%";
+//           if (pxl < 410) {
+//             if (pxl < 350) {
+//               d.style.width = "83%";
+//             } else {
+//               d.style.width = "70%";
+//             }
+//           e.style.marginLeft = "10%";
+//             }
+//         }
+//   }
+let i = 0;
 function menuFxn() {
   let pxl = window.innerWidth;
-  let a = document.getElementsByClassName("icon")[0];
-  let b = document.getElementsByClassName("main-nav")[0];
-  let c = document.getElementsByClassName("burg")[0];
-  let d = document.getElementsByClassName("name")[0];
-  let e = document.getElementsByClassName("logo")[0];
-      if (b.style.display === "block") {
-          a.style.width = "15%";
-          b.style.display = "none";
-          b.style.background = "none";
-          c.style.maxWidth = "100%";
-          if (pxl < 410) {
-          d.style.width = "initial";
-          e.style.marginLeft = "auto";
-            }
+  let a = document.getElementById("JS1");
+  let b = document.getElementById("JS2");
+  let c = document.getElementById("JS3");
+  let d = document.getElementById("JS4");
+  let e = document.getElementById("JS5");
+  i += 1;
+      if (i % 2 == 0) {
+          a.classList.remove("iconjs");
+          b.classList.remove("main-navjs");
+          c.classList.remove("burgjs");
+            if (pxl < 410) {
+                d.classList.remove("namejs");
+                e.classList.remove("logojs");
+              }
         } else {
-          a.style.width = "25%";
-          b.style.display = "block";
-          b.style.background = "black";
-          b.style.borderRadius  = "15px";
-          c.style.maxWidth = "60%";
-          if (pxl < 410) {
-            if (pxl < 350) {
-              d.style.width = "83%";
-            } else {
-              d.style.width = "70%";
-            }
-          e.style.marginLeft = "10%";
-            }
+          a.classList.add("iconjs");
+          b.classList.add("main-navjs");
+          c.classList.add("burgjs");
+            if (pxl < 410) {
+                d.classList.add("namejs");
+                e.classList.add("logojs");
+              }
         }
   }
